ee606c92944e0cef81aa3bac3ebf61d678778914
musicman <ermilov.official@bk.ru>
2020-10-27 09:38:40 -0400

    No vector, matrix has worked

--- 03/main.cpp
+++ 03/main.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
-#include "vector.hpp"
+#include "matrix.hpp"
 
 int main()
 {
+    int64_t** arr1 = new int64_t*[2];
+    arr1[0] = new int64_t[3] {1, 2, 3};
+    arr1[1] = new int64_t[3] {4, 5, 6};
+
+    int64_t** arr2 = new int64_t*[2];
+    arr2[0] = new int64_t[3] {1, -3, 5};
+    arr2[1] = new int64_t[3] {0, 1, -4};
+    
+    Matrix A(2, 3, arr1);
+    Matrix B(2, 3, arr2);
+
+    std::cout << "A = " << A << "\n";
+    std::cout << "B = " << B << "\n";
+
+    Matrix C = A + B;
+
+    std::cout << "C = A + B = " << C << "\n";
+
+    std::cout << "A[1][2] = " << A[1][2] << "\n";
+    std::cout << "A[1][2] the = 784\n";
+    A[1][2] = 784;
+    std::cout << "A[1][2] = " << A[1][2] << "\n";
+    
     // Three is will be tests =)
 
+    /*Vector* vect1 = new Vector[3] {Vector(2), Vector(2), Vector(2)};
+    Vector* vect2 = new Vector[3] {Vector(2), Vector(2), Vector(2)}; 
 
-    Vector v1(3, new int64_t[3] {1, 2, 3}), v2(3, new int64_t[3] {3, -4, 5});
+    Matrix m1(vect1), m2(vect2);
+
+    //Matrix m3 = m1 + m2;
+
+    delete[] vect1;
+    delete[] vect2;
+
+    /*Vector v1(3, new int64_t[3] {1, 2, 3}), v2(3, new int64_t[3] {3, -4, 5});
     std::cout << "v1 = " << v1 << "\nv2 = " << v2 << '\n';
 
     Vector v_sum = v1 + v2;
@@ -32,7 +64,7 @@ int main()
 
     v1[2] = 54;
     std::cout << "Now v1[2] = 54, v1 =  " << v1 << '\n';
-    std::cout << "Get v2[0]: " << v2[0];
+    std::cout << "Get v2[0]: " << v2[0];*/
 
     return 0;
 }
--- 03/matrix.hpp
+++ 03/matrix.hpp
@@ -1,44 +1,141 @@
 #pragma once
 
 #include <ostream>
-#include "vector.hpp"
 
-class Matrix : Vector
+class Matrix
 {
 public:
+    Matrix(u_int16_t _rows, u_int16_t _columns): rows(_rows), columns(_columns)
+    {
+        std::cout << "Mat constr 1" << std::endl;
 
-    Matrix(u_int16_t _rows, u_int16_t _columns): rows(_rows), columns(_columns) {};
+        data = new int64_t*[_rows];
+        for (size_t i = 0; i < _rows; i++) data[i] = new int64_t[_columns];
+    }
+
+    Matrix(u_int16_t _rows, u_int16_t _columns, int64_t** _data): rows(_rows), columns(_columns), data(_data)
+    {
+        std::cout << "Mat constr 2" << std::endl;
+    }
+
+    ~Matrix()
+    {
+        std::cout << "Mat destr" << std::endl;
+        
+        for (size_t i = 0; i < rows; i++) { delete[] data[i]; }
+        delete[] data;
+    }
 
     u_int16_t getRows() const { return rows; }
     u_int16_t getColumns() const { return columns; }
 
-    Matrix& operator+(Matrix& mat);
+    Matrix operator+(const Matrix& mat)
+    {
+        if ((rows != mat.rows) || (columns != mat.columns)) throw "Can't sum matrixes with different shapes!\n";
 
-    Matrix& operator*=(int64_t num);
+        int64_t** new_data = new int64_t*[rows];
+        for (size_t i = 0; i < rows; i++) new_data[i] = new int64_t[columns];
 
-    bool operator==(Matrix& mat);
+        for (size_t i = 0; i < rows; i++)
+        {
+            for (size_t j = 0; j < columns; j++)
+            {
+                new_data[i][j] = data[i][j] + mat.data[i][j];
+            }
+        }
+
+        return Matrix(rows, mat.columns, new_data);
+        }
+
+    Matrix& operator*=(int64_t num)
+    {
+        for (size_t i = 0; i < rows; i++)
+        {
+            for (size_t j = 0; j < columns; j++)
+            {
+                data[i][j] *= num;
+            }
+        }
+
+        return *this;
+    }
+
+    bool operator==(Matrix& mat)
+    {
+        bool isEqual = true;
+
+        for (size_t i = 0; i < rows; i++)
+        {
+            for (size_t j = 0; j < columns; j++)
+            {
+                if (data[i][j] != mat.data[i][j])
+                {
+                    isEqual = false;
+                    break;
+                }
+            }
+            if (!isEqual) break;
+        }
+
+        return isEqual;
+    }
 
     bool operator!=(Matrix& mat) { return !operator==(mat); }
 
-    int64_t& operator[](const size_t index);
-
-    std::ostream show(Matrix& mat);
-    
-    ~Matrix()
+    private: class Proxy;
+    public:
+    Proxy operator[](const size_t index) const
     {
-        Vector v;
-        // ???
-        for (size_t i = 0; i < rows; i++)
-        {
-            delete[] matrix[i];
-        }
-        
-        delete[] matrix;
+        if (rows < index) throw std::out_of_range("");
+
+        return Proxy(index, columns, data);
     }
 
+    friend std::ostream& operator<<(std::ostream& stream, const Matrix& m);
+
 private:
-    int64_t** matrix;
+    class Proxy
+    {
+    public:
+        Proxy(size_t _row_index, u_int16_t _columns, int64_t** _data): 
+            row_index(_row_index), columns(_columns), data(_data) {}
+        
+        int64_t& operator[](const size_t column_index)
+        {
+            if (columns < column_index) throw std::out_of_range("");
+
+            return data[row_index][column_index];
+        }
+
+        const int64_t& operator[](const size_t column_index) const
+        {
+            if (columns < column_index) throw std::out_of_range("");
+
+            return data[row_index][column_index];
+        }
+    private:
+        int64_t** data;
+
+        u_int16_t columns;
+        size_t row_index;
+    };
+
+    int64_t** data;
 
     u_int16_t rows;
     u_int16_t columns;
 };
+
+std::ostream& operator<<(std::ostream& stream, const Matrix& m)
+{
+    for (size_t i = 0; i < m.rows; i++)
+    {
+        stream << '\n';
+        for (size_t j = 0; j < m.columns; j++)
+        {
+            stream << m.data[i][j] << " ";
+        }
+    }
+
+    return stream;      
+}

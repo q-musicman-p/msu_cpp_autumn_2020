697a27c312c293810c3fbcf565db0be81938a659
musicman <ermilov.official@bk.ru>
2020-11-27 10:14:26 -0500

    all constructors

--- 07/main.cpp
+++ 07/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
 #include "vector.hpp"
+#include <vector>
+
+void constrTest()
+{
+    Vector<int> v1(10);
+    Vector<int> v2(10, -234);
+    Vector<double> v5(new double[3] { 1.2, 3, 5.4}, 3);
+    Vector<int> v3(v1);
+    Vector<int> v4(std::move(v1));
+}
+
+void equalOperatorsTest()
+{
+    Vector<double> v1(new double[3] { 1.2, 3, 5.4}, 3);
+    Vector<double> v2(new double[3] { 0, -2.6, 9.24}, 3);
+    Vector<double> v3(new double[3] { -6, 2, 1.566}, 3);
+
+    v2 = v1;
+
+    v2 = std::move(v3);
+}
 
 int main()
 {
     //tests
+
+    //constrTest(); //ok, but need testing with operator[]
+    //equalOperatorsTest();
+
+
+    /*
+    auto v1 = std::vector<int> {1, 2, 3, 4, 5};
+
+    auto v2 = std::vector<u_int16_t> {1, 2, 3, 4, 5};
+
+    v2 = v1;
+
+    std::cout << "size = " << v.size() << "\ncapasity = " << v.capacity() << std::endl;
+
+    v.push_back(6);
+
+    std::cout << "size = " << v.size() << "\ncapasity = " << v.capacity() << std::endl;*/
 
     return 0;
 }
--- 07/vector.hpp
+++ 07/vector.hpp
@@ -1,16 +1,22 @@
 #pragma once
 
-template <class T, class Iterator, class Allocator>
+#include <iostream>
+
+template <class T>
 class Vector
 {
+private:
     size_t size_;
     size_t capacity_;
     T* memory_;
+
+    class Iterator;
+    class Allocator;
 public:
-    Vector(T* array, size_t size);
     explicit Vector(size_t size);
     Vector(size_t size, const T& default_value);
-
+    Vector(T* array, size_t size);
+    
     Vector(const Vector& other);
     Vector(Vector&& other);
 
@@ -31,10 +37,10 @@ public:
     template <class... Args>
     void emplace_back(Args&&... args); //universal referense
 
-    bool empty() { return size_ == 0; } const noexcept;
+    bool empty() const noexcept { return size_ == 0; } 
 
-    size_t size() { return size_; } const noexcept;
-    size_t capacity() { return capacity_; } const noexcept;
+    size_t size() const noexcept { return size_; } 
+    size_t capacity() const noexcept { return capacity_; } 
 
     void clear() noexcept;
 
@@ -54,3 +60,65 @@ public:
 
     void reverse(size_t capacity);
 };
+
+// ======================================
+// ============ CONSTRUCTORS ============
+// ======================================
+
+template <class T>
+Vector<T>::Vector(size_t size): size_(size), capacity_(size), memory_(new T[size]) { std::cout << "C_size" << std::endl; }
+
+template <class T>
+Vector<T>::Vector(size_t size, const T& default_value): size_(size), capacity_(size), memory_(new T[size])
+{
+    std::cout << "C_size_def" << std::endl;
+
+    //std::fill(begin(), end(), T());
+    std::fill(memory_, memory_ + size_, default_value);
+    //for (size_t i = 0; i < size; i++)
+    //{
+    //    memory_[i] = T();
+    //}
+}
+
+template <class T>
+Vector<T>::Vector(T* array, size_t size): size_(size), capacity_(size), memory_(array) {std::cout << "C_arr" << std::endl;}
+
+template <class T>
+Vector<T>::Vector(const Vector& other): size_(other.size_), capacity_(other.size_), memory_(new T[other.size_])
+{
+    std::cout << "C_copy" << std::endl;
+
+    //std::copy(other.begin(), other.end(), memory_);
+    std::copy(other.memory_, other.memory_ + other.size_, memory_);
+}
+
+template <class T>
+Vector<T>::Vector(Vector&& other): size_(other.size_), capacity_(other.size_)
+{
+    std::cout << "C_rvalue" << std::endl;
+
+    memory_ = new(other.memory_) T[other.size_];
+
+    other.size_ = 0;
+    other.capacity_ = 0;
+    other.memory_ = nullptr;
+}
+
+template <class T>
+Vector<T>::~Vector()
+{
+    std::cout << "Destr" << std::endl;
+
+    delete[] memory_;
+}
+
+// ======================================
+// ============= OPERATORS ==============
+// ======================================
+
+template <class T>
+Vector<T>& Vector<T>::operator=(const Vector& other)
+{
+
+}
